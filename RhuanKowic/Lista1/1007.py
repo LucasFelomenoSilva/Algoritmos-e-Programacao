28551bd6a9b675c269f77dd760855a0904d2c509
Rhuan Kowic Santos <rhuanksantos2@souunilavras.com>
2022-10-08 16:22:51 -0300

    Arquivos da lista1 do Rhuan inseridos

---
+++ RhuanKowic/Lista1/1007.py
@@ -0,0 +1,7 @@
+#https://www.beecrowd.com.br/judge/pt/problems/view/1007
+A = int(input(""))
+B = int(input(""))
+C = int(input(""))
+D = int(input(""))
+DIFERENCA = (A * B - C * D)
+print("DIFERENCA = %s" %DIFERENCA)
