7bec2fbf66d6c35387a01264428eefc35441ad7d
Rhuan Kowic Santos <rhuanksantos2@souunilavras.com>
2022-10-08 15:42:26 -0300

    Arquivos da lista1 do Rhuan inseridos

---
+++ RhuanKowicSantos/Lista1/1005.py
@@ -0,0 +1,6 @@
+#https://www.beecrowd.com.br/judge/pt/problems/view/1005
+A = float(input(""))
+B = float(input(""))
+peso = (A * 3.5) + (B * 7.5)
+media = peso / 11
+print('MEDIA = {:.5f}'.format(media))
---
+++ RhuanKowicSantos/Lista1/1006.py
@@ -0,0 +1,7 @@
+#https://www.beecrowd.com.br/judge/pt/problems/view/1006
+A = float(input(""))
+B = float(input(""))
+C = float(input(""))
+peso = (A*2)+(B*3)+(C*5)
+media = peso/10
+print("MEDIA = %.1f" %media)
